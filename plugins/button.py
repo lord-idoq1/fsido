d86a7d47c1964be2699d0fc2942b88ce4129aa43
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2023-01-21 14:52:48 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -62,7 +62,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="GASS NONTON",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
@@ -80,7 +80,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="GASS NONTON",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
@@ -99,7 +99,7 @@ def fsub_button(client, message):
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text="ᴄᴏʙᴀ ʟᴀɢɪ",
+                        text="GASS NONTON",
                         url=f"https://t.me/{client.username}?start={message.command[1]}",
                     )
                 ]
